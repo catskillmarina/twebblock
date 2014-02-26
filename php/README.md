d2e65dfbf148587d08e794bdb9f6b4486e3e43e6
Marina Brown <catskillmarina@gmail.com>
2014-02-27 00:05:12 +0100

    Matching in php working

--- php/README.md
+++ php/README.md
@@ -1,6 +1,14 @@
 The php script requires the url for any and all ACL's used for blocking
 
+php5-curl must be installed for curl to work.
+
 file://whatever
 http://whatever
 https://whatever
 ftp://whatever
+
+From the curl manpage - curl can handle the following protocols:
+
+DICT, FILE, FTP, FTPS, GOPHER, HTTP, HTTPS,  IMAP,
+IMAPS,  LDAP,  LDAPS,  POP3, POP3S, RTMP, RTSP, SCP, SFTP, SMTP, SMTPS,
+TELNET and TFTP
--- php/block.php
+++ php/block.php
@@ -4,40 +4,84 @@
 <BODY BGCOLOR="FFFFFF">
 <?php
 $referrer = $_SERVER['HTTP_REFERER'];
-echo $referrer;
+echo "<h1>referrer: ", $referrer, "</h1><hr>\n";
 
-$acl_urls = array("file:///home/marina/public_html/twebblock/ACL");
+$acl_urls = array(
+    "file:///home/marina/public_html/twebblock/ACL",
+    );
 
-$badreferrers = array("/http:\/\/stratus\.e271\.net/","/godhates\/.fags\/com/");
+$badreferrers = array();
 
 foreach ($acl_urls as &$acl)
 {
-#   curl the contents from each $acl_urls
-#   split the curled contents into an array
-    
+    echo "<h1>acl: $acl</h1>";
+    #---------------------------------------#
+    # curl the contents from each $acl_urls #
+    #---------------------------------------#
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $acl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $curloutput = curl_exec($ch);
+    curl_close($ch); 
 
     $temparray = array();
+
+    $patterns = array();
+    $patterns[0] = '/^#.+$/';
+    $patterns[1] = '/^$/';
+    $patterns[2] = '/http:\/\//i';
+    $patterns[3] = '/https:\/\//i';
+    $patterns[4] = '/\//';
+    $patterns[5] = '/\./';
+    $replacements = array();
+    $replacements[0] = '';
+    $replacements[1] = '';
+    $replacements[2] = '';
+    $replacements[3] = '';
+    $replacements[4] = '\/';
+    $replacements[5] = '\.';
+    $curlclean = preg_replace($patterns, $replacements, $curloutput);
+
+
+    $curlresults = preg_split("/\R/",$curlclean);
     foreach ($curlresults as &$ref)
     {
-        if (preg_match("/^#/", $ref))
-        {
-            next;
-        }
-        if (preg_match("/^$/", $ref))
-        {
-            next;
-        }
-        # remove http:// and https://
-        # escape '.'s
-        # escape '/'s
-        # add [http|https] to beginning of strings
         # think about FTP urls
-        array_push ($temparray, $ref);
+        if(!preg_match("/#/", $ref)&&!preg_match("/^$/", $ref))
+        {
+            array_push ($temparray, $ref);
+        }
     }
-    array_merge($badreferrers, $temparray);
+
+    # add [http|https] to beginning of strings
+    $patterns = array();
+    $patterns[0] = '/^/';
+    $patterns[1] = '/$/';
+    $replacements = array();
+    $replacements[0] = '/[http|https]:\/\/';
+    $replacements[1] = '/';
+    $temparraymod = preg_replace($patterns, $replacements, $temparray);
+
+#   echo "<h1>BEFORE temparraymod PRINT</h1>\n";
+#   $i = 0;
+#   foreach($temparraymod as &$foo)
+#   {
+#       echo "<font color =\"00ff00\">$i: $foo</font><br>\n";
+#       $i++;
+#   }
+    $badrefarray = array_merge($badreferrers, $temparraymod);
 }
 
-foreach ($badreferrers as &$badref)
+
+echo "<h1>BEFORE $badrefarray PRINT</h1>\n";
+$i = 0;
+foreach($badrefarray as &$foo)
+{
+    echo "<font color =\"0000ff\">$i: $foo</font><br>\n";
+    $i++;
+}
+
+foreach ($badrefarray as &$badref)
 {
     echo "<br>";
     echo $badref;
