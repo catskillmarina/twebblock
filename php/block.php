4b505a3438751cc24084872ac13dff62f7a1d94e
marina <marina@Debian-60-squeeze-32-minimal.(none)>
2014-02-26 14:50:56 +0100

    Start on PHP

--- php/block.php
+++ php/block.php
@@ -3,7 +3,28 @@
 
 <BODY BGCOLOR="FFFFFF">
 <?php
-echo $_SERVER['HTTP_REFERER'];
+$referrer = $_SERVER['HTTP_REFERER'];
+echo $referrer;
+
+$acl_urls = array("file:///home/marina/public_html/twebblock/ACL");
+#
+# Need code to curl the ACL's and put them in one array
+#
+
+$badreferrers = array("/http:\/\/stratus\.e271\.net/","/godhates\/.fags\/com/");
+foreach ($badreferrers as &$badref)
+{
+    echo "<br>";
+    echo $badref;
+    if (preg_match($badref, $referrer))
+    {
+        echo "<br>Match";
+    }
+    else
+    {
+        echo "<br>No Match";
+    } 
+}
 ?>
 </BODY>
 </HTML>
