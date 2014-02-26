0c3a6b6e59081462cb93c46fd7cd72ea607c7166
Marina Brown <catskillmarina@gmail.com>
2014-02-26 21:32:51 +0100

    More work on php

--- php/block.php
+++ php/block.php
@@ -7,11 +7,36 @@ $referrer = $_SERVER['HTTP_REFERER'];
 echo $referrer;
 
 $acl_urls = array("file:///home/marina/public_html/twebblock/ACL");
-#
-# Need code to curl the ACL's and put them in one array
-#
 
 $badreferrers = array("/http:\/\/stratus\.e271\.net/","/godhates\/.fags\/com/");
+
+foreach ($acl_urls as &$acl)
+{
+#   curl the contents from each $acl_urls
+#   split the curled contents into an array
+    
+
+    $temparray = array();
+    foreach ($curlresults as &$ref)
+    {
+        if (preg_match("/^#/", $ref))
+        {
+            next;
+        }
+        if (preg_match("/^$/", $ref))
+        {
+            next;
+        }
+        # remove http:// and https://
+        # escape '.'s
+        # escape '/'s
+        # add [http|https] to beginning of strings
+        # think about FTP urls
+        array_push ($temparray, $ref);
+    }
+    array_merge($badreferrers, $temparray);
+}
+
 foreach ($badreferrers as &$badref)
 {
     echo "<br>";
